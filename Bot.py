43fee9846ab8fd4fdc9cc55e32c351261ecbdb43
unknown <ndinhledan@gmail.com>
2018-12-19 14:23:58 +0800

    Added spreadsheet manipulation

--- Bot.py
+++ Bot.py
@@ -2,6 +2,8 @@ import discord
 import time
 import datetime
 import asyncio
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 ''' TODO unready
 pee
@@ -11,7 +13,12 @@ current status
 mention
 '''
 
-TOKEN = "NTE5ODIzMzM3MTA3NzUwOTEy.Duk65g.BQQ5Zp42G1E-SngTHW73ATaZzts"
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client_sheet = gspread.authorize(creds)
+sheet = client_sheet.open('Credit Score').sheet1
+
+TOKEN = "NTE5ODIzMzM3MTA3NzUwOTEy.DvYYjg.oInz1LWhIH7P1MGSmK373kcutCY"
 
 client = discord.Client()
 
@@ -27,6 +34,10 @@ status_list = [
     ]
 
 
+command_list = [
+    "hello", "mem", "prefix", "schedule", "ready", "unready", "cancel"
+]
+
 help_text = {
     "schedule": '''Try **<prefix>schedule <time>**
     Example: **<prefix>schedule 2 hourse 20 minutes** 
@@ -37,16 +48,23 @@ I can't believe you are this dumb, how can you be in nova land
 
     Example: **<prefix>prefix duh!
     Use it wisely, I'm old, I'm not used to changes.
-    '''
+    ''',
+    "add_credit": "**" + settings["prefix"] + '''add_credit <person> <amount>**
+Example: **''' + settings["prefix"] + '''add_credit TerroristBot 100**''',
+    "minus_credit": "**" + settings["prefix"] + '''minus_credit <person> <amount>**
+Example: **''' + settings["prefix"] + '''minus_credit TerroristBot 100**''',
+
+
 }
 
 pre_text = '''
 
-Use ''' + settings["prefix"]+ '''**schedule <time>** to schedule your battle
+Use **''' + settings["prefix"]+ '''schedule <time>** to schedule your battle
 For example ** ''' + settings["prefix"] + '''schedule 2 hours 20 minutes**
 
 My current prefix is ''' + settings["prefix"] + '''
 To change prefix use **<prefix>prefix <new prefix>**
+
 '''
 
 @client.event
@@ -66,11 +84,9 @@ async def on_message(message):
             msg = "How dare you say hello to me. I am your general. Show some respect. {0.author.mention} do 50 push ups for me".format(message)
             await client.send_message(message.channel, msg)
             
-        if m.startswith(settings["prefix"] + "em"):
-            em = discord.Embed(title='Title', description='My Embed Content.', colour=0xDEADBF)
-            em.add_field(name ='Yes', value='Haha', inline=False)
-            em.add_field(name ='No', value='HoHo', inline=True)
-            await client.send_message(message.channel, embed=em, content = "Hello you fuck")
+        if m.startswith(settings["prefix"] + "link"):
+            em = discord.Embed(title='Link to Credit Score', colour=0xDEADBF, url="https://docs.google.com/spreadsheets/d/1CNsWa3avZbJRTpsxBGGy-iKTEaQ9__WaX_Oe3UNVd1Q/edit?usp=sharing")
+            await client.send_message(message.channel, embed=em)
             
         if m.startswith(settings["prefix"] + "mem"):
             for mem in message.channel.server.members:
@@ -80,26 +96,29 @@ async def on_message(message):
                 await client.send_message(message.channel, msg)
 
         if m == (settings["prefix"] + "help"):
-            await client.send_message(message.channel, pre_text)
+            command_str = ""
+            for command in command_list:
+                command_str += (command + "   ")
+            msg = pre_text + '''**```Current commands```**```''' + command_str + "```"
+            await client.send_message(message.channel, msg)
 
         if m == (settings["prefix"] + "ready"):
-            for status in status_list:
-                if message.channel == status["channel"] and status["schedule"] == True:
-                    if status["mems"][message.author] == False:
-                        status["mems"][message.author] = True
-                        await client.send_message(message.channel, "{0.mention} has finished getting high on laughing gas.".format(message.author) ,embed=create_em_list_sol(status["mems"]))
-                    else:
-                        await client.send_message(message.channel, "{0.mention} Easy now boy. No need to be so eager".format(message.author) ,embed=create_em_list_sol(status["mems"]))    
+            status = find_channel_status(message.channel)
+            if status["schedule"] == True:
+                if status["mems"][message.author] == False:
+                    status["mems"][message.author] = True
+                    await client.send_message(message.channel, "{0.mention} has finished getting high on laughing gas.".format(message.author) ,embed=create_em_list_sol(status["mems"]))
+                else:
+                    await client.send_message(message.channel, "{0.mention} Easy now boy. No need to be so eager".format(message.author) ,embed=create_em_list_sol(status["mems"]))    
         
         if m == (settings["prefix"] + "unready"):
-            for status in status_list:
-                if message.channel == status["channel"] and status["schedule"] == True:
-                    if status["mems"][message.author] == True:
-                        status["mems"][message.author] = False
-                        await client.send_message(message.channel, "{0.mention} decided he has enough and is gonna go grab some more laughing gas.".format(message.author) ,embed=create_em_list_sol(status["mems"]))
-                    else:
-                        await client.send_message(message.channel, "{0.mention} Don't try to be a smartass.".format(message.author) ,embed=create_em_list_sol(status["mems"]))
-                    break    
+            status = find_channel_status(message,channel)
+            if status["schedule"] == True:
+                if status["mems"][message.author] == True:
+                    status["mems"][message.author] = False
+                    await client.send_message(message.channel, "{0.mention} decided he has enough and is gonna go grab some more laughing gas.".format(message.author) ,embed=create_em_list_sol(status["mems"]))
+                else:
+                    await client.send_message(message.channel, "{0.mention} Don't try to be a smartass. You haven't readied yet".format(message.author) ,embed=create_em_list_sol(status["mems"]))
 
         if m.startswith(settings["prefix"] + "prefix"):
             dmsg, tmsg = m.split("prefix")
@@ -111,8 +130,67 @@ async def on_message(message):
             settings["prefix"] = new_prefix
             await client.send_message(message.channel, "Congrats. Your new prefix is **"+ settings["prefix"]+ "**. Don't forget.")
 
+        if m == (settings["prefix"] + "status"):
+            status = find_channel_status(message.channel)
+            if status["schedule"] == True:
+                time_remaining = status["time"] - int(time.time()) 
+                res = [0,0,0] #hour, minute, second
+                res[0] = time_remaining // 3600
+                res[1] = (time_remaining - res[0]*3600)//60
+                res[2] = (time_remaining - res[0]*3600 - res[1]*60)
+                await client.send_message(message.channel, "Battle will commence in about {} hours {} minutes {} seconds".format(res[0], res[1], res[2]), embed=create_em_list_sol(status["mems"]))
+            else:
+                await client.send_message(message.channel,"There is currently no scheduled battle. So booooooooring!")
+
+        if m == (settings["prefix"] + "cancel"):
+            status = find_channel_status(message.channel)
+            if status["schedule"] == True :
+                status["schedule"] = False
+                await client.send_message(message.channel, "You have canceled your scheduled battle! What a pussy.")
+            else :
+                await client.send_message(message.channel, "Huh?")
+
+        if m == (settings["prefix"] + "reset_credit"):
+            count = 2
+            for mem in message.channel.server.members:
+                if mem == client.user:
+                    continue
+                sheet.update_cell(count, 1, mem.name)
+                sheet.update_cell(count, 3, '1000')
+                print(mem.name)
+                count +=1
+
+        if m.startswith(settings["prefix"] + "add_credit"):
+            dmsg = m[len(settings["prefix"] + "add_credit"):].strip()
+            splits = dmsg.split(" ")
+            if not splits or len(splits) < 2:
+                return await send_help_text(client, message.channel, "add_credit")
+            addee = splits[0]
+            amount = splits[1]
+            try:
+                cell = sheet.find(addee)
+                sheet.update_cell(cell.row, 3, str(int(sheet.cell(cell.row, 3).value) + int(amount)))
+                await client.send_message(message.channel, addee + " has been awarded " + amount + " points by the ultimate leader " + message.author.name + "for exceptional bravery")
+            except gspread.exceptions.CellNotFound:
+                await client.send_message(message.channel, "Member not found")
+            
+        if m.startswith(settings["prefix"] + "minus_credit"):
+            dmsg = m[len(settings["prefix"] + "minus_credit"):].strip()
+            splits = dmsg.split(" ")
+            if not splits or len(splits) < 2:
+                return await send_help_text(client, message.channel, "minus_credit")
+            addee = splits[0]
+            amount = splits[1]
+            try:
+                cell = sheet.find(addee)
+                sheet.update_cell(cell.row, 3, str(int(sheet.cell(cell.row, 3).value) - int(amount)))
+                await client.send_message(message.channel, addee + " has been punished for " + amount + " points by the ultimate leader " + message.author.name + "for being a pussy")
+            except gspread.exceptions.CellNotFound:
+                await client.send_message(message.channel, "Member not found")
+            
+
         if m.startswith(settings["prefix"] + "schedule"):
-            msg = "You have called for your comrads to go to battle in {}. Type **" + settings["prefix"] + "ready** after you finished wearing your diapers.".format(m[len("t!schedule"):].strip())
+            msg = "You have called for your comrads to go to battle in {}. Type **{}ready** after you have finished wearing your diapers.".format(m[len(settings["prefix"] + "schedule"):].strip(), settings["prefix"])
             listsol = ""
             
             mems = {}
@@ -121,7 +199,7 @@ async def on_message(message):
                     continue
                 listsol += "{0.mention}: Not Ready\n".format(mem)
                 mems[mem] = False
-            em = discord.Embed(title ='Soldiers', description=listsol)
+            em = discord.Embed(title ='Soldiers', description=listsol, colour=0xDEADBF)
 
             dmsg, tmsg = m.split("schedule")
 
@@ -176,7 +254,7 @@ def create_em_list_sol(mems):
             listsol += "{0.mention}: Ready\n".format(mem)
         else:
             listsol += "{0.mention}: Not Ready\n".format(mem)
-    em = discord.Embed(title ='Soldiers', description=listsol)
+    em = discord.Embed(title ='Soldiers', description=listsol, colour=0xDEADBF)
     return em
 
 def check_all_ready(status):
@@ -185,6 +263,11 @@ def check_all_ready(status):
             return False
     return True
 
+def find_channel_status(channel):
+    for status in status_list:
+        if status["channel"] == channel:
+            return status
+    return None
 
 async def check_reminders(client):
     await client.wait_until_ready()
